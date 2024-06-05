acd87331d184ff3c107ccbf9703e9c3028f3f20a
Tausif Islam Sheik <you@example.com>
2024-06-05 14:39:31 +0600

    Category cards added

--- js/front-page.js
+++ js/front-page.js
@@ -7,15 +7,44 @@ const handleCategory = async () =>{
       const div = document.createElement('div');
       div.innerHTML = `
 
-      <button class="btn btn-outline hover:bg-red-600">${category.category}</button>
+      <button onclick="handleCategoryData('${category.category_id}')" class="btn btn-outline hover:bg-red-600">${category.category}</button>
       `;
 
       categoryContainer.appendChild(div);
     })
     
-    console.log(data.data)
+    
 
 }
+
+const handleCategoryData = async (categoryId) =>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+  const data = await res.json();
+   const cardContainer = document.getElementById('card-container');
+   data.data.forEach(card =>{
+    console.log(card)
+    const div = document.createElement('div');
+    div.innerHTML = `
+     <figure><img src="${card?.thumbnail}" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        Shoes!
+        <div class="badge badge-secondary">NEW</div>
+      </h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <div class="badge badge-outline">Fashion</div> 
+        <div class="badge badge-outline">Products</div>
+      </div>
+    </div> 
+    `
+    cardContainer.appendChild(div);
+   })
+}
+
+
+
+
 
 handleCategory();
 
