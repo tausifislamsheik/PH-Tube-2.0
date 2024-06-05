90b26d6d851c29799c11c384214e6dccd5b0a675
Tausif Islam Sheik <you@example.com>
2024-06-05 19:19:59 +0600

    No data found option added

--- js/front-page.js
+++ js/front-page.js
@@ -6,8 +6,11 @@ const handleCategory = async () =>{
     data.data.forEach(category =>{
       const div = document.createElement('div');
       div.innerHTML = `
-
+      
       <button onclick="handleCategoryData('${category.category_id}')" class="btn btn-outline hover:bg-red-600">${category.category}</button>
+      
+    </div>
+      
       `;
 
       categoryContainer.appendChild(div);
@@ -20,27 +23,42 @@ const handleCategory = async () =>{
 const handleCategoryData = async (categoryId) =>{
   const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
   const data = await res.json();
+  const cardDetails = data.data;
    const cardContainer = document.getElementById('card-container');
+   const emptyContainer = document.getElementById('empty-data-container');
    cardContainer.innerHTML = "";
-   data.data?.forEach(card =>{
-    console.log(card)
-    const div = document.createElement('div');
-    div.innerHTML = `
-     <figure><img class="h-[200px] rounded-lg" src="${card?.thumbnail}" alt="Shoes" /></figure>
-    <div class="flex m-4 gap-2">
-       <div>
-        <img class="rounded-full w-12 h-12" src="${card?.authors[0]?.profile_picture}" alt="Shoes" />
-       </div>
-       <div>
-         <p class="font-bold">${card?.title}</p>
-         <h1>${card?.authors[0]?.profile_name} <span>${card.authors[0].verified ? '<i class="fa-solid fa-circle-check text-blue-500 ml-2"></i>' : ""}</span></h1>
-         <p>${card?.others.views}</p>
-       </div>
-
-    </div> 
-    `
-    cardContainer.appendChild(div);
-   })
+   emptyContainer.innerHTML = "";
+   if(cardDetails.length > 0){
+    cardDetails?.forEach(card =>{
+      console.log(card)
+      const div = document.createElement('div');
+      div.innerHTML = `
+       <figure><img class="h-[200px] rounded-lg" src="${card?.thumbnail}" alt="Shoes" /></figure>
+      <div class="flex m-4 gap-2">
+         <div>
+          <img class="rounded-full w-12 h-12" src="${card?.authors[0]?.profile_picture}" alt="Shoes" />
+         </div>
+         <div>
+           <p class="font-bold">${card?.title}</p>
+           <h1>${card?.authors[0]?.profile_name} <span>${card.authors[0].verified ? '<i class="fa-solid fa-circle-check text-blue-500 ml-2"></i>' : ""}</span></h1>
+           <p>${card?.others.views}</p>
+         </div>
+  
+      </div> 
+      `
+      cardContainer.appendChild(div);
+     })
+   }else{
+      const div = document.createElement('div');
+      div.innerHTML = `
+      <div class="text-center my-28 space-y-5">
+      <img src="images/Icon.png" alt="" class="mx-auto">
+      <h3 class="text-2xl font-bold">Oops!! Sorry, There is no <br> content here</h3>
+      </div>
+      `
+      emptyContainer.appendChild(div);
+   }
+   
 }
 
 
@@ -49,4 +67,8 @@ const handleCategoryData = async (categoryId) =>{
 
 handleCategory();
 handleCategoryData("1000");
+
+
+
+
 
